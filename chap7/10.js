29ca07563c898045f96a76920f1d2a1414873ef7
robin-mi <robin.seu@foxmail.com>
2018-05-11 13:53:36 +0800

    10.js

---
+++ chap7/10.js
@@ -0,0 +1,10 @@
+function Student(val){
+    this.name = val;
+}
+function Teacher(val){
+    var name = val;
+}
+var s = new Student("robin");
+console.log(s.name);//robin
+var t = new Teacher("robin");
+console.log(t.name);//undefined
