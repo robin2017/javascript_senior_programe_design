4cf379a2d82740722d3b4f3acaa73b77692ec0ea
robin-mi <robin.seu@foxmail.com>
2018-05-13 08:48:31 +0800

    DD

---
+++ chap22/2.js
@@ -0,0 +1,6 @@
+var person = {name:"robin"};
+console.log(Object.isExtensible(person));//true
+//不可扩展执行
+Object.preventExtensions(person);
+//不可扩展检测
+console.log(Object.isExtensible(person));//false
