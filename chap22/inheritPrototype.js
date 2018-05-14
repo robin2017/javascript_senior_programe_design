25e36a2e3357831aa8832da20021aa1bba68000b
robin2017 <robin.seu@foxmail.com>
2018-05-14 22:47:32 +0800

    dd

--- chap22/inheritPrototype.js
+++ chap22/inheritPrototype.js
@@ -1,11 +1,12 @@
-function inheritPrototype(subType,superType) {
-  var prototype = object(superType.prototype);
-  prototype.constructor = subType;
-  subType.prototype = prototype;
+function inheritPrototype(subType, superType) {
+    var prototype = object(superType.prototype);
+    prototype.constructor = subType;
+    subType.prototype = prototype;
 }
-function object(o){
-  function F(){}
-  F.prototype = o;
-  return new F();
+function object(o) {
+    function F() {}
+
+    F.prototype = o;
+    return new F();
 }
 exports.inheritPrototype = inheritPrototype;
